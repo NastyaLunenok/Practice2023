99e6b0d42894e1c7d5f60c11e4fa84b1debcd656
Nastya Lunenok <nastya.lunenok@gmail.com>
2023-07-12 14:57:14 +0800

    Pt_3_5: list of numbers that are not prime

---
+++ Pt_3_5.py
@@ -0,0 +1,11 @@
+lower_value = int(input("Введите нижнюю границу диапазона: ")) 
+upper_value = int(input("Введите верхнюю границу диапазона: "))
+listt=[]
+for number in range(lower_value, upper_value + 1):
+    if number > 1:
+        for i in range(2, number):
+            if(number % i) == 0:
+                listt.append(number)
+                break 
+
+print("Cписок из чисел, которые не являются простыми", listt)

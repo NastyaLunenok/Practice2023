2203603606daee5d56e82a3ab0117924db37530f
Nastya Lunenok <nastya.lunenok@gmail.com>
2023-07-12 19:31:46 +0800

    Pt_5_1: file with books

---
+++ Pt_5_1.py
@@ -0,0 +1,13 @@
+import csv
+
+books = [("Book", "Author", "Year of release"),
+         ["Murder on the Orient Express", "Agatha Christie", "1934"],
+         ["The Kite Runner", "Khaled Hosseini", "2003"],
+         ["A Game of Thrones", "George R. R. Martin", "1996"],
+         ["Atonement", "Ian McEwan", "2001"],
+         ["A Brief History of Time", "Stephen Hawking", "1988"]]
+
+with open("books.csv", "w", newline='') as file:
+    writer = csv.writer(file, delimiter=";")
+    writer.writerows(books)
+    

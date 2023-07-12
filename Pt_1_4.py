ce4ae183381ef30e28f177a329193802a9f3834b
Nastya Lunenok <nastya.lunenok@gmail.com>
2023-07-12 14:16:20 +0800

    Pt_1_4: checking range

---
+++ Pt_1_4.py
@@ -0,0 +1,15 @@
+print("Введите число от 10 до 20:")
+flag=False
+while not(flag):
+    number=int(input())
+    if number<10:
+        print("Число меньше требуемого диапазона. Введите число ещё раз:")
+        flag=False
+    elif number>20:
+        print("Число больше требуемого диапазона. Введите число ещё раз:")
+        flag=False
+    else:
+        flag=True
+        print("Спасибо")
+  
+      

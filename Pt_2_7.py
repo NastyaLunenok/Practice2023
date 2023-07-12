c963a9a48097eefd4668a63b1127154eafb5589e
Nastya Lunenok <nastya.lunenok@gmail.com>
2023-07-12 14:39:08 +0800

    Pt_2_7: sum negative numbers

---
+++ Pt_2_7.py
@@ -0,0 +1,10 @@
+flag=True
+summ=0
+print("Программа считает сумму отрицательных чисел. Вводите числа")
+while flag:
+    number=int(input())
+    if number<0:
+        summ+=number
+    else:
+        flag=False
+print("Сумма отрицательных чисел:", summ) 

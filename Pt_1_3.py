076ed92a5193e93917a9a2174c7a6c20c4869d20
Nastya Lunenok <nastya.lunenok@gmail.com>
2023-07-12 14:16:19 +0800

    Pt_1_3: comparing numbers

---
+++ Pt_1_3.py
@@ -0,0 +1,8 @@
+print("Введите 3 числа")
+mas=[]
+for i in range(3):
+    mas.append(int(input()))
+mas.sort(reverse=True)
+print("Наибольшее число:",max(mas))
+print("Числа в порядке убывания:", mas)
+               

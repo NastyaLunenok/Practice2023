93a64a97d0ebf0a8a807e3ada58cca4588c1f6c9
Nastya Lunenok <nastya.lunenok@gmail.com>
2023-07-12 14:57:13 +0800

    Pt_3_10: dictionary of letters

---
+++ Pt_3_10.py
@@ -0,0 +1,11 @@
+print("Введите строку")
+string=input().lower()
+dic={}
+for i in range (len(string)):
+    if string[i]!=' ':
+        if string[i]in dic.keys():
+            dic[string[i]]+=1
+        else:
+            dic[string[i]]=1
+    
+print(dic)

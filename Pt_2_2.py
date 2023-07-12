e9a489282cf9a3676f6b7c23c108060bbeb06bcf
Nastya Lunenok <nastya.lunenok@gmail.com>
2023-07-12 14:39:03 +0800

    Pt_2_2: TV programs

---
+++ Pt_2_2.py
@@ -0,0 +1,8 @@
+TV_programs=[" Орёл и решка", "Голос", "Танцы", "Импровизвция"]
+print(*[element + '\n' for element in TV_programs])
+print("Введите название передачи")
+title=input()
+print("Введите позицию передачи в списке")
+position=int(input())
+TV_programs.insert(position-1, title)
+print(*[element + '\n' for element in TV_programs])

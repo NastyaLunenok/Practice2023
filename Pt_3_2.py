61cc743a3b68fb5740689be975b26f2e4d6dc513
Nastya Lunenok <nastya.lunenok@gmail.com>
2023-07-12 14:57:14 +0800

    Pt_3_2: fibonacci numbers

---
+++ Pt_3_2.py
@@ -0,0 +1,16 @@
+from functools import reduce
+print("Введите число:")
+flag=False
+while not(flag):
+    n=int(input())
+    if n<0:
+        print("Ошибка ввода. Введите положительное число:")
+    else:
+        flag=True
+        
+listt=[1,1]
+while listt[1]<=n:
+    print(listt[1])
+    x=reduce(lambda x, y: x+y,listt)
+    listt[0]=listt[1]
+    listt[1]=x

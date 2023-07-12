a62aa2b56f20f738443b24e21c3861762b1eb50f
Nastya Lunenok <nastya.lunenok@gmail.com>
2023-07-12 22:29:52 +0800

    Pt_5_1: file with books

--- Pt_5_1.py
+++ Pt_5_1.py
@@ -1,6 +1,6 @@
 import csv
 
-books = [("Book", "Author", "Year of release"),
+books = [("Книга", "Автор", "Год выпуска"),
          ["Murder on the Orient Express", "Agatha Christie", "1934"],
          ["The Kite Runner", "Khaled Hosseini", "2003"],
          ["A Game of Thrones", "George R. R. Martin", "1996"],

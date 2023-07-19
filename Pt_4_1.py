cca46a27968e799ce134e94a5f60376b14a67791
Nastya Lunenok <nastya.lunenok@gmail.com>
2023-07-19 21:02:02 +0800

    inserted missing whitespaces around operators and removed whitespaces in an empty line

--- Pt_4_1.py
+++ Pt_4_1.py
@@ -1,10 +1,10 @@
-digit=int(input("Введите число: "))
-num=list(str(abs(digit)))
-if digit>0:
+digit = int(input("Введите число: "))
+num = list(str(abs(digit)))
+if digit > 0:
     num.sort(reverse=True)
     print("Наибольшее число:", "".join(num))
-    
+
 else:
     num.sort()
-    num_int=int("".join(num))*(-1)
+    num_int = int("".join(num)) * (-1)
     print("Наибольшее число:", num_int)

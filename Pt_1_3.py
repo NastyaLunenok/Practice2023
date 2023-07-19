98f030f30c1237b5a4ff7233f8e1f1ac7a1eb292
Nastya Lunenok <nastya.lunenok@gmail.com>
2023-07-19 18:40:30 +0800

    inserted missing spaces around operators

--- Pt_1_3.py
+++ Pt_1_3.py
@@ -1,8 +1,7 @@
 print("Введите 3 числа")
-mas=[]
+mas = []
 for i in range(3):
     mas.append(int(input()))
 mas.sort(reverse=True)
-print("Наибольшее число:",max(mas))
+print("Наибольшее число:", max(mas))
 print("Числа в порядке убывания:", mas)
-               

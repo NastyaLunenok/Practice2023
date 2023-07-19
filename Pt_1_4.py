6edae9cf3550e94b5b850ff5a93a6940dd4a9dc1
Nastya Lunenok <nastya.lunenok@gmail.com>
2023-07-19 18:48:11 +0800

    inserted missing whitespaces around operators and removed the whitespaces in the empty lines

--- Pt_1_4.py
+++ Pt_1_4.py
@@ -1,15 +1,13 @@
 print("Введите число от 10 до 20:")
-flag=False
-while not(flag):
-    number=int(input())
-    if number<10:
+flag = False
+while not (flag):
+    number = int(input())
+    if number < 10:
         print("Число меньше требуемого диапазона. Введите число ещё раз:")
-        flag=False
-    elif number>20:
+        flag = False
+    elif number > 20:
         print("Число больше требуемого диапазона. Введите число ещё раз:")
-        flag=False
+        flag = False
     else:
-        flag=True
+        flag = True
         print("Спасибо")
-  
-      

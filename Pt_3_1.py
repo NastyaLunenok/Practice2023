95a07f8fe9ab3075eba2249a4710da9d4696e98b
Nastya Lunenok <nastya.lunenok@gmail.com>
2023-07-19 20:12:51 +0800

    inserted missing whitespaces around operators

--- Pt_3_1.py
+++ Pt_3_1.py
@@ -1,8 +1,8 @@
 print("Введите длину списка чисел")
-n=int(input())
+n = int(input())
 print("Введите", n, "чисел")
-number=[]
+number = []
 for i in range(n):
     number.append(int(input()))
-average_value=lambda x: sum(x)/len(x)
+average_value = lambda x: sum(x) / len(x)
 print(average_value(number))

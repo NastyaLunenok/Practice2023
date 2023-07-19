074617bfa9b14d2fc4e82df4cbf60489bdb038c0
Nastya Lunenok <nastya.lunenok@gmail.com>
2023-07-19 18:49:39 +0800

    inserted missing whitespaces around operators

--- Pt_1_5.py
+++ Pt_1_5.py
@@ -1,9 +1,9 @@
-digit=int(input())
-if digit==3:
+digit = int(input())
+if digit == 3:
     print("выигрыш")
-elif digit==0:
+elif digit == 0:
     print("проигрыш")
-elif digit==1:
+elif digit == 1:
     print("ничья")
 else:
     print("неизвестный результат")

64bad2760a579815f74a845d28eeb001dfd86337
Nastya Lunenok <nastya.lunenok@gmail.com>
2023-07-19 19:31:10 +0800

    inserted missing whitespaces around operators

--- Pt_2_4.py
+++ Pt_2_4.py
@@ -1,15 +1,14 @@
 print("Введите в строку коэффициенты a, b, c уравнения ax^2+bx+c=0")
-a, b, c=map(int, input().split())
-D=b**2-4*a*c
-if D<0:
+a, b, c = map(int, input().split())
+D = b**2 - 4 * a * c
+if D < 0:
     print("Корней нет")
-elif D==0:
+elif D == 0:
     print("Один корень:")
-    x=-b/(2*a)
-    print("x =",x)
+    x = -b / (2 * a)
+    print("x =", x)
 else:
     print("Два корня:")
-    x1=(-b+D**(0.5))/(2*a)
-    x2=(-b-D**(0.5))/(2*a)
+    x1 = (-b + D**(0.5)) / (2 * a)
+    x2 = (-b - D**(0.5)) / (2 * a)
     print(" x1 =", x1, "\n", "x2 =", x2)
-

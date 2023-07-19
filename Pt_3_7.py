a751049daec5fdbf0736843db96b2b148a6f715d
Nastya Lunenok <nastya.lunenok@gmail.com>
2023-07-19 20:42:14 +0800

    inserted missing whitespaces around operators, reduced the number of characters in the string to the norm and removed whitespaces in an empty line

--- Pt_3_7.py
+++ Pt_3_7.py
@@ -1,15 +1,15 @@
 print("Введите строку")
-consonants=["б", "в", "г", "д", "ж", "з", "й", "к", "л", "м", "н", "п", "р", "с", "т", "ф", "х", "ц", "ч", "ш", "щ"]
-vowels=["а", "е", "ё", "и", "о", "у", "ы", "э", "ю", "я"]
-string=input().lower()
-dic={}
+consonants = ["б", "в", "г", "д", "ж", "з", "й", "к", "л", "м", "н",
+              "п", "р", "с", "т", "ф", "х", "ц", "ч", "ш", "щ"]
+vowels = ["а", "е", "ё", "и", "о", "у", "ы", "э", "ю", "я"]
+string = input().lower()
+dic = {}
 for i in range(len(string)):
     if string[i] in dic.keys():
         pass
     else:
         if string[i] in consonants:
-            dic[string[i]]=False
+            dic[string[i]] = False
         if string[i] in vowels:
-            dic[string[i]]=True
-print(dic)
-        
+            dic[string[i]] = True
+print(dic) 

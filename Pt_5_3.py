723ba56a97ed28242c8b267fb3433b9552ca51c9
Nastya Lunenok <nastya.lunenok@gmail.com>
2023-07-19 21:45:55 +0800

    reduced the number of characters in the string to the norm and removed whitespaces in an empty line

--- Pt_5_3.py
+++ Pt_5_3.py
@@ -10,8 +10,8 @@ def is_valid_year(year):
             return False
     except ValueError:
         return False
-    
-    
+
+
 def search_books(from_year, to_year):
     books = []
     with open("books.csv", newline='') as file:
@@ -21,19 +21,21 @@ def search_books(from_year, to_year):
                 year = str(year)
                 if year == row["Год выпуска"]:
                     books.append(row)
-                    
+      
     if not books:
         print("В списке нет книг этих лет")
     else:
         for row in books:
             print(row["Книга"], "|", row["Автор"], "|", row["Год выпуска"])
-   
-    
+
+
 fl = True
 while fl:
     start_year = input("Введите нижнюю границу диапазона годов: ")
     end_year = input("Введите верхнюю границу диапазона годов: ")
-    if not is_valid_year(start_year) or not is_valid_year(end_year) or start_year > end_year:
+    if (not is_valid_year(start_year) or
+        not is_valid_year(end_year) or
+        start_year > end_year):
         print("Некорректный ввод годов. Введите еще раз.")
     else:
         search_books(int(start_year), int(end_year))

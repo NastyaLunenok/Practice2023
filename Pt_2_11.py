555847ba9e65f495ad8280808a4abafee4b687c6
Nastya Lunenok <nastya.lunenok@gmail.com>
2023-07-19 19:46:46 +0800

    inserted missing whitespaces around operators

--- Pt_2_11.py
+++ Pt_2_11.py
@@ -1,6 +1,6 @@
 print("Введите отрезок целых чисел [a,b]")
-a,b=map(int, input().split())
-summ=0
-for i in range(a,b+1):
-    summ+=i
+a, b = map(int, input().split())
+summ = 0
+for i in range(a, b + 1):
+    summ += i
 print("Сумма всех целых чисел от a до b:", summ)

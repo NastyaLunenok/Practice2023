214f6563dd6ab7107cb6e9d41960b52ae4d17e80
Nastya Lunenok <nastya.lunenok@gmail.com>
2023-07-19 20:50:10 +0800

    inserted missing whitespaces around operators and removed whitespaces in an empty line

--- Pt_3_10.py
+++ Pt_3_10.py
@@ -1,11 +1,11 @@
 print("Введите строку")
-string=input().lower()
-dic={}
-for i in range (len(string)):
-    if string[i]!=' ':
-        if string[i]in dic.keys():
-            dic[string[i]]+=1
+string = input().lower()
+dic = {}
+for i in range(len(string)):
+    if string[i] != ' ':
+        if string[i] in dic.keys():
+            dic[string[i]] += 1
         else:
-            dic[string[i]]=1
-    
+            dic[string[i]] = 1
+
 print(dic)

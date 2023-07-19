579060e24137f83ee3d6fd8aeacc0737a0076db5
Nastya Lunenok <nastya.lunenok@gmail.com>
2023-07-19 19:44:33 +0800

    inserted missing whitespaces around operators and reduced the number of characters in the string to the norm

--- Pt_2_9.py
+++ Pt_2_9.py
@@ -1,8 +1,10 @@
 print("Введите натуральное число, не содержащее одинаковых цифр")
-number=int(input())
-digits=list(map(int,str(number)))
-max_value=max(digits)
-max_index_start=digits.index(max_value)
+number = int(input())
+digits = list(map(int, str(number)))
+max_value = max(digits)
+max_index_start = digits.index(max_value)
 print("Максимальная цифра:", max_value)
-print("Порядковый номер максимальной цифры, считая от начала:", max_index_start+1)
-print("Порядковый номер максимальной цифры, считая от конца:", len(digits)-max_index_start)
+print("Порядковый номер максимальной цифры, считая от начала:",
+      max_index_start + 1)
+print("Порядковый номер максимальной цифры, считая от конца:",
+      len(digits) - max_index_start)

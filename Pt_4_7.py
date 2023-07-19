ed99e7c5aa52b3cf2e51b1e95af940a80eabf4e1
Nastya Lunenok <nastya.lunenok@gmail.com>
2023-07-19 21:24:08 +0800

    inserted missing whitespaces around operators

--- Pt_4_7.py
+++ Pt_4_7.py
@@ -1,6 +1,7 @@
 import itertools
-n=int(input("Введите длину списка: "))
-listt=[]
+
+n = int(input("Введите длину списка: "))
+listt = []
 print("Введите значения элементов списка:")
 for i in range(n):
     listt.append(input())

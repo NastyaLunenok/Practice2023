04abe7a81623e56f1fef2c76414d114530213598
Nastya Lunenok <nastya.lunenok@gmail.com>
2023-07-19 20:44:58 +0800

    inserted missing whitespaces around operators and added the final line

--- Pt_3_8.py
+++ Pt_3_8.py
@@ -1,8 +1,8 @@
-alph="абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
-dic={}
-i=1
+alph = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+dic = {}
+i = 1
 for letter in alph:
-    dic[letter]=i
-    i+=1
+    dic[letter] = i
+    i += 1
 
 print(dic)

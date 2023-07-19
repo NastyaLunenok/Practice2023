38be5160b0fd4407c2a35a93e99ae02c301170d2
Nastya Lunenok <nastya.lunenok@gmail.com>
2023-07-19 18:58:07 +0800

    inserted missing whitespaces around operators

--- Pt_1_8.py
+++ Pt_1_8.py
@@ -1,7 +1,7 @@
 print("Введите в строку ваше имя и возраст:")
-name, age=map(str, input().split())
-if (1<=(int(age)%10)<=4) and (int(age)<10 or int(age)>20):
-    string="Привет, "+name+"! Тебе уже "+age+" года."
+name, age = map(str, input().split())
+if (1 <= (int(age) % 10) <= 4) and (int(age) < 10 or int(age) > 20):
+    string = "Привет, " + name + "! Тебе уже " + age + " года."
 else:
-    string="Привет, "+name+"! Тебе уже "+age+" лет."
+    string = "Привет, " + name + "! Тебе уже " + age + " лет."
 print(string)
